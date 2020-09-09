50a5ad276f47a40f1a7ad19a5610d1983579d4c5
LucSedirae <jondeavers@gmail.com>
2020-09-09 01:56:49 -0400

    Quiz start timer and quiz main function are now working

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,36 @@
 var startButton = document.querySelector("#startButton"); 
 var time = document.querySelector("#time");
-var timeLeft = 5;
+var countdown = document.querySelector("#countdown");
+var timeLeft 
 
 startButton.addEventListener("click", startTimer);
 
 function startTimer() {
-  setInterval(function(){
-        if(timeLeft <= 0) {
-            clearInterval(timeLeft=0);
+    var startCount = 5;
+    setInterval(function(){
+        if(startCount <= 0) {
+            clearInterval(startCount=0);
             startQuiz();
         }
-      time.innerHTML = timeLeft;
-    timeLeft -=1;
-  }, 1000)
+        countdown.innerHTML = startCount;
+        startCount -=1;
+    }, 1000);
+    return;
 }
     
-
-
 function startQuiz() {
-    timeLeft = 60;
-    document.getElementById("quizQuestion").innerHTML = "What is the sum of 2 + 2?";
+    timeLeft = 10;
+    setInterval(function(){
+        if(timeLeft <= 0) {
+            clearInterval(timeLeft=0);
+            gameOver();
+        }
+        time.innerHTML = timeLeft;
+        timeLeft -=1;
+    }, 1000)
+    return;
+}
+
+function gameOver() {
+    document.getElementById("quizQuestion").innerHTML = "GAME OVER";
 }
